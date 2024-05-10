73c2bb20a16349c615dd5099924e7ce892f0c58a
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-10 19:25:08 +0530

    create routes and components

---
+++ src/providers/data/index.tsx
@@ -0,0 +1,42 @@
+//Setting data provider from stratch that will gonna sue in entire application
+import graphqlDataProvider, {
+  GraphQLClient,
+  liveProvider as graphqlLiveProvider,
+} from "@refinedev/nestjs-query";
+import { createClient } from "graphql-ws";
+import { fetchWrapper } from "./fetch-wrapper";
+
+export const API_BASE_URL = "https://api.crm.refine.dev";
+export const API_URL = `${API_BASE_URL}/graphql`; //calling the data from thi api
+export const WS_URL = "wss://api.crm.refine.dev/graphql";
+
+export const client = new GraphQLClient(API_URL, {
+  fetch: (url: string, options: RequestInit) => {
+    try {
+      return fetchWrapper(url, options);
+    } catch (error) {
+      return Promise.reject(error as Error);
+    }
+  },
+});
+
+export const wsClient =
+  typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient
+  ? graphqlLiveProvider(wsClient)
+  : undefined;
---
+++ src/providers/index.ts
@@ -0,0 +1,2 @@
+export * from "./data"; //export all the files from data folder
+export * from "./auth";
