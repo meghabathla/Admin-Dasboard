1425a3127cc5ce43adfaa2ddb58fa1ec5fa32bcd
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-11 20:23:53 +0530

    creating header and user data, account setting

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Avatar as AntdAvatar, AvatarProps } from "antd";
+import { getNameInitials } from "@/utilities";
 
 type Props = AvatarProps & {
-  name: string;
+  name?: string;
 };
 
 const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
     <AntdAvatar
-      alt={"Javascript Mastery"}
+      alt={name}
       size="small"
       style={{
         backgroundColor: "#87d068",
         display: "flex",
         alignItems: "center",
         border: "none",
+        ...style,
       }}
+      {...rest}
     >
-      {name}
+      {getNameInitials(name || "")}
     </AntdAvatar>
   );
 };
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -3,11 +3,48 @@ import React from "react";
 import CustomAvatar from "../custom-avatar";
 import { useGetIdentity } from "@refinedev/core";
 
-
-import typr {User} from '@graphql/schema.types'
+import type { User } from "@/graphql/schema.types";
+import { Text } from "../text";
+import { SettingOutlined } from "@ant-design/icons";
+import { AccountSettings } from "./account-setting";
 
 const CurrentUser = () => {
-    const {data :user}= useGetIdentity<User>()//getting this from auth.tsx
+  const { data: user } = useGetIdentity<User>(); //getting this from auth.tsx
+  const [opened, setOpened] = React.useState(false);
+
+  const content = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Text strong style={{ padding: "12px 20px" }}>
+        {user?.name}
+      </Text>
+
+      <div
+        style={{
+          borderTop: "1px solid #d9d9d9",
+          padding: "4px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <Button
+          style={{ textAlign: "left" }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setOpened(true)}
+        >
+          Account settings
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Popover
@@ -15,9 +52,22 @@ const CurrentUser = () => {
         trigger="click"
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
+        content={content}
       >
-        <CustomAvatar />
+        <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size="default"
+          style={{ cursor: "pointer" }}
+        />
       </Popover>
+      {user && (
+        <AccountSettings
+          opened={opened}
+          setOpened={setOpened}
+          userId={user.id}
+        />
+      )}
     </>
   );
 };
