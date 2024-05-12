c7a4bf10a0cbf223524cb3ad86a3ca735f3b6b6e
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-12 22:13:03 +0530

    create compoenents fro Home page

---
+++ src/components/home/deals-chart.tsx
@@ -0,0 +1,30 @@
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+
+import { Text } from "../text";
+
+const DealsChart = () => {
+  return (
+    // <Card
+    //   style={{ height: "100%" }}
+    //   headStyle={{ padding: "8px 16px" }}
+    //   title={
+    //     <div
+    //       style={{
+    //         display: "flex",
+    //         alignItems: "center",
+    //         gap: "8px",
+    //       }}
+    //     >
+    //       <DollarOutlined />
+    //       <Text size="sm" style={{ marginLeft: "0.5rem" }}></Text>
+    //     </div>
+    //   }
+    // >
+    //   {" "}
+    //   DealsChart
+    // </Card>
+  );
+};
+
+export default DealsChart;
--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import { Badge, Card, List } from "antd";
 import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
 import { Text } from "../text";
