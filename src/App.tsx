f08a4efc8c0bd8620a7d4bf19212a726764351e8
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-15 14:32:54 +0530

    added route create and edit fro company page

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import { Create } from "./pages/companyList/create";
+import EditPage from "./pages/companyList/edit";
 
 function App() {
   return (
@@ -56,7 +58,11 @@ function App() {
                 }
               >
                 <Route index element={<Home />} />
-                <Route path="/companies" element={<CompanyList />} />
+                <Route path="/companies">
+                  <Route index element={<CompanyList />} />
+                  <Route path="new" element={<Create />} />
+                  <Route path="edit/:id" element={<EditPage />} />
+                </Route>
               </Route>
             </Routes>
             {/* <RefineKbar /> */}
