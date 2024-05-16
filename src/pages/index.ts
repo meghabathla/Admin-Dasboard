2edd7889c91850e970a130b7e6f3527245e08d2c
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-16 23:38:13 +0530

    export files from here

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,7 @@ export * from "./home";
 export * from "./forgotPassword";
 export * from "./login";
 export * from "./register";
+export * from "./companyList/index";
+export * from "./companyList/create";
+export * from "./companyList/edit";
+export * from "./tasks/list";
